a56fef3868d964736cc7f709dff24ffa8055e7fd
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2021-04-03 16:06:59 +0200

    refactoring + player - dash + input handler

---
+++ Assets/Scripts/Player/InputHandler.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThePackt{
+    public class InputHandler : MonoBehaviour
+    {
+       private Werewolf _player;
+       private bool _allowInput = true; //true until we implement aniamtor
+       private Vector2 _movement;
+
+
+
+
+
+        private void Start()
+        {
+            _player = GetComponent<Werewolf>();
+        }
+
+
+        private void Update()
+        {
+            
+            if (_allowInput) //true until we implement aniamtor
+            {
+                HandleMovementInput(Time.deltaTime);
+
+                HandleDashInput(Time.deltaTime);
+
+                HandleJumpInput(Time.deltaTime);
+
+               //TODO  HandleAttacks(Time.deltaTime);
+
+            }
+        }
+
+         private void HandleMovementInput(float delta)
+        {
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.y = 0;
+            _movement.Normalize();
+            _player.SetDirection(_movement);
+            _player.SetIsMoving(true);
+            //TODO animator trigger
+        }
+
+        private void HandleDashInput(float delta){
+
+            if (Input.GetKey(KeyCode.Space)){ //TODO adding stamina??? not a good idea to dash-spamming.
+                _player.SetIsDashing(true);
+                //TODO set trigger like : _animatorHandler.ActivateTargetTrigger("Dash");
+                //if stamina then spend it here.
+            }
+        }
+
+        private void HandleJumpInput(float delta){
+            Debug.Log("is pressing w?");
+            if(Input.GetKeyDown(KeyCode.W) && _player.GetIsGrounded()){ //Stamina??
+                Debug.Log("yes!");
+                _player.SetIsJumping(true);
+                //if stamina then spend it here
+                //TODO set animator trigger
+            } 
+            else Debug.Log("no");
+            
+        }
+
+
+    }
+}
--- Assets/Scripts/Player/Werewolf.cs
+++ Assets/Scripts/Player/Werewolf.cs
@@ -15,10 +15,26 @@ namespace ThePackt{  //to be used in every class
         #region variables  
         protected Rigidbody2D _rb; // put '_' before every protected or private variable
         protected Animator _anim;
+        [SerializeField] private GameObject _sprite;
         [SerializeField] protected float _speed; //use serializefield if you want to initialize the variable from unity's inspector.
+
+        [SerializeField] protected float _dashMultiplier;
         [SerializeField] protected float _jumpForce;
         private Vector2 _direction;
+        private bool _isFacingLeft = true;
         private bool _isGrounded = true;
+        private bool _isDashing = false;
+        private bool _isMoving = false;
+        private bool _isJumping = false;
+        private bool _isCrouching = false;
+        private bool _isUsingWereWolfBaseAttack = false;
+        private bool _isUsingBaseHumanAttack = false;
+        private bool _isUsingSpecialAttack = false;
+        private bool _isUsingItem = false;
+
+    
+   
+       
 
         #endregion
 
@@ -32,13 +48,10 @@ namespace ThePackt{  //to be used in every class
         /* 
          * Implements the standard movement
          */
-        private void Move()  //method name always uppercase
-        {
-            transform.position += new Vector3(Input.GetAxis("Horizontal"),0,0) * Time.deltaTime * _speed;
-        }
-
+       
         private void Jump()  //method name always uppercase
         {
+            Debug.Log("jumping");
             _isGrounded = false;
             _rb.AddForce(transform.up * _jumpForce, ForceMode2D.Impulse);
         }
@@ -47,38 +60,153 @@ namespace ThePackt{  //to be used in every class
         #endregion 
 
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             _rb = gameObject.GetComponent<Rigidbody2D>();
             //to be done later :  _anim = gameObject.GetComponent<Animator>();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            Move();
 
-            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        private void FixedUpdate()
+        {    
+            
+            if (_isDashing)
             {
-                Debug.Log("jump");
-                Jump();
+                UpdateSpriteDirection(_direction);
+                UpdatePosition(_direction * _speed * _dashMultiplier, Time.fixedDeltaTime);
+                _isDashing = false;
             }
+            else if (_isJumping){
+                //UpdateSpriteDirection(_direction);
+                Jump();
+                _isJumping = false;
+            }
+            else if(_isMoving)
+            {
+                UpdateSpriteDirection(_direction);
+                UpdatePosition(_direction * _speed, Time.fixedDeltaTime);
+                _isMoving = false;
+            }
+            
         }
 
-        void OnCollisionEnter2D(Collision2D collision)
+
+        private void UpdatePosition(Vector2 movement, float delta)
+        { 
+            if (movement.magnitude != 0)
+                _rb.velocity = Vector3.zero; 
+            //_rb.velocity = movement * _speed * delta;
+            transform.position += new Vector3(movement.x,movement.y,0) * Time.deltaTime * _speed;
+            //_rb.MovePosition(_rb.position + movement * delta);       
+         }
+
+         private void UpdateSpriteDirection(Vector3 movement)
+        {
+            if (movement.x > 0 && _isFacingLeft ||
+                movement.x < 0 && !_isFacingLeft)
+                Flip();
+        }
+
+        private void Flip()
+        {
+            if(_isFacingLeft){
+                _sprite.transform.rotation = Quaternion.Euler(0,180,0);
+            }
+            else{
+                _sprite.transform.rotation = Quaternion.Euler(0,0,0);     
+            }
+            _isFacingLeft = !_isFacingLeft;
+        }
+
+
+
+        private void OnCollisionEnter2D(Collision2D collision)
         {
             Debug.Log(collision.collider.gameObject.name);
-            _isGrounded = true;
+            _isGrounded = true; //Here you are considering every collision, 
+                                //that means that if the player collides with something which is not "terrain" will be considered grounded
         }
 
         #endregion
+  
+
+
 
         #region getter
+        public bool GetIsDashing(){
+            return _isDashing;
+        }
+
+        public bool GetIsJumping(){
+            return _isJumping;
+        }
+
+        public bool GetIsMoving(){
+            return _isMoving;
+        }
+
+        public bool GetIsCrouching(){
+            return _isCrouching;
+        }
+
+        public bool GetIsUsingWereWolfBaseAttack(){
+            return _isUsingWereWolfBaseAttack;
+        }
+
+        public bool GetUsingHumanBaseAttack(){
+            return _isUsingBaseHumanAttack;
+        }
+
+        public bool GetIsUsingSpecialAttack(){
+            return _isUsingSpecialAttack;
+        }
+
+        public bool GetIsUsingItem(){
+            return _isUsingItem;
+        }
+
+        public bool GetIsGrounded(){
+            return _isGrounded;
+        }
 
         #endregion 
 
         #region setter
+        public void SetIsDashing(bool value){
+            _isDashing = value;
+        }
 
+        public void SetIsJumping(bool value){
+             _isJumping = value;
+        }
+
+        public void SetIsMoving(bool value){
+            _isMoving = value;
+        }
+
+        public void SetIsCrouching(bool value){
+             _isCrouching = value;
+        }
+
+        public void SetIsUsingWereWolfBaseAttack(bool value){
+             _isUsingWereWolfBaseAttack = value;
+        }
+
+        public void SetUsingHumanBaseAttack(bool value){
+             _isUsingBaseHumanAttack = value;
+        }
+
+        public void SetIsUsingSpecialAttack(bool value){
+             _isUsingSpecialAttack = value;
+        }
+
+        public void SetIsUsingItem(bool value){
+             _isUsingItem = value;
+        }
+
+        public void SetDirection(Vector2 direction){
+            _direction = direction;
+        }
         #endregion
     }
 }
