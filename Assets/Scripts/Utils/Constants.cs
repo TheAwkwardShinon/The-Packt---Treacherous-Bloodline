b732bf03a4d7b8fd500f23eaee257f654a1c6faa
Federico Locatelli <federicolocatelli1@hotmail.it>
2021-04-24 20:27:24 +0200

    adds variant prefabs and their spawn system

--- Assets/Scripts/Utils/Constants.cs
+++ Assets/Scripts/Utils/Constants.cs
@@ -5,6 +5,17 @@ namespace ThePackt
 {
     public class Constants
     {
+        #region attacks
         public const string BASE = "base";
+        #endregion
+
+        #region factions
+        public const string CEUIN = "ceuin";
+        public const string MOONSIGHTERS = "moonsighters";
+        public const string HERIN = "herin";
+        public const string NATURIA = "naturia";
+        public const string FELE = "fele";
+        public const string AYATANA = "ayatana";
+        #endregion 
     }
 }
