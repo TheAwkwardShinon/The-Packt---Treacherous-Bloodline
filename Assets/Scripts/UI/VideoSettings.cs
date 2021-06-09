e1b78921991924c462544f39a70c0efa3007be2c
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2021-06-09 03:15:29 +0200

    logo

---
+++ Assets/Scripts/UI/VideoSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThePackt{
+    public class VideoSettings : MonoBehaviour
+    {
+
+        public void SetFullScreen(){
+            Screen.fullScreen = true;
+        }
+    }
+}
